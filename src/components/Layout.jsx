9af3aa6f6e888ae8dedfbb44f6410b43c65e098e
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-10-19 23:11:08 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,15 @@
 import BottomNav from './BottomNav.jsx';
+import ThemeToggle from './ThemeToggle.jsx';
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-base-200">
+      <header className="navbar bg-base-100 shadow-sm sticky top-0 z-40">
+        <div className="flex-1 px-2 font-semibold">TRICINTY ⚡</div>
+        <div className="flex-none px-2">
+          <ThemeToggle />
+        </div>
+      </header>
       <main className="flex-grow p-4 pb-24">{children}</main>
       <BottomNav />
     </div>
